49625bd331fddc6b66f995a8e4620cb49a1b69ee
voidwalker77 <noccomposer@gmail.com>
2024-09-21 06:07:14 -0300

    fix(vercel): fix

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,20 +1,20 @@
 /// <reference types="cypress" />
 
-Cypress.Commands.add("getCurrentDate", () => {
-    const currentDate = new Date();
+Cypress.Commands.add('getCurrentDate', () => {
+    const currentDate = new Date()
 
     const options: Intl.DateTimeFormatOptions = {
-        weekday: "long",
-        day: "2-digit",
-        month: "long",
-        year: "numeric",
-    };
+        weekday: 'long',
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+    }
 
-    const formattedDate = new Intl.DateTimeFormat("pt-BR", options).format(
-        currentDate
-    );
+    const formattedDate = new Intl.DateTimeFormat('pt-BR', options).format(
+        currentDate,
+    )
 
     return cy.wrap(
-        formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1)
-    );
-});
+        formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1),
+    )
+})
